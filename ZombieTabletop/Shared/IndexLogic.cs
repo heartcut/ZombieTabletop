cc562608e458109b23b8dbe4ce89049640142e85
Storytime Sensei <ihateblizzardcs@gmail.com>
2022-10-05 18:03:35 +0900

    Testing

--- ZombieTabletop/Shared/IndexLogic.cs
+++ ZombieTabletop/Shared/IndexLogic.cs
@@ -15,9 +15,7 @@
                 int dice = rndm.Next(1,dicesides+1);
 
                 totRoll += dice;
-            }
-
-            //testing
+            }           
 
             return totRoll;
         }
