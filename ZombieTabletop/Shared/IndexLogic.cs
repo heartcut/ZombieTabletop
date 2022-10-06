400fb14083c9611573eaa5878d36275fb9356a1f
Storytime Sensei <ihateblizzardcs@gmail.com>
2022-10-06 17:34:27 +0900

    Advantage/Disavantage Method

--- ZombieTabletop/Shared/IndexLogic.cs
+++ ZombieTabletop/Shared/IndexLogic.cs
@@ -3,21 +3,66 @@
     public static class IndexLogic
     {
 
-        public static int GenerateDiceRolls(int amount,int dicesides)
+        public static int GenerateDiceRolls(int amount,int dicesides, int AdvantageType)
         {
             
             Random rndm = new Random();
-
-            int totRoll = 0;
-
-            for (int i=0; i < amount; i++)
+            
+            int resultRoll = 0;
+            
+            if(AdvantageType == 1)
             {
-                int dice = rndm.Next(1,dicesides+1);
 
-                totRoll += dice;
-            }         
-            return totRoll;
-        }
+                for (int i = 0; i < 2; i++)
+                {
+                    int totRoll = 0;
+                    for (int d = 0; d < amount; d++)
+                    {
+                        int dice = rndm.Next(1, dicesides + 1);                       
+                        totRoll += dice;
+                    }
+
+                    if (resultRoll < totRoll)
+                    {
+                       resultRoll = totRoll;
+                       
+                    }
+                }
+                return resultRoll;
+            }
+            
+            if(AdvantageType == 0) 
+            {
+
+                for (int i=0; i < amount; i++)
+                {
+                    int dice = rndm.Next(1,dicesides+1);
+                    resultRoll += dice;                    
+                }         
+                return resultRoll;
+            }
+
+            if (AdvantageType == -1)
+            {
+                resultRoll = 10000;
+                for (int i = 0; i < 2; i++)
+                {
+
+                    int totRoll = 0;
+                    for (int d = 0; d < amount; d++)
+                    {
+                        int dice = rndm.Next(1, dicesides + 1);                        
+                        totRoll += dice;                        
+                    }
+                    if (resultRoll > totRoll)
+                    {
+                        resultRoll = totRoll;                       
+                    }
+                }
+                return resultRoll;
+            }
+            return 0;
+        }       
 
     }
 }
